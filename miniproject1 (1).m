a705457fed52bb10c13a911c7cf647d1ebd900f9
sarahaly231-hub <sarahaly231@gmail.com>
2026-03-24 21:20:34 +0200

    Add files via upload

---
+++ miniproject1 (1).m
@@ -0,0 +1,107 @@
+disp("Welcome to signal generation program :)..")
+BP = input("Enter no. of breaking points");
+   x= input("Enter the starting time");
+    l= input("Enter the ending time");
+    F= input("Enter the frequency");
+T = linspace(x,l,(l-x)*F);
+Y = [];  
+PS=[x  l];
+ps1=[ ];
+for i= 1:BP
+    ps= input ("Enter the position");
+    ps1=[ps1  ps];
+    PS=[x ps1 l];
+end 
+i=1;
+while i<=BP+1
+   
+    disp("1.DC 2.Ramp 3.general 4.exponential 5.sinusoidal");
+    S=input('Enter the type of the signal');
+    x=PS(i);
+    l=PS(i+1);
+     t= linspace(x,l,(l-x)*F);
+    if S==1
+       amp= input("Enter the amplitude");
+       y=zeros(1,(l-x)*F)+amp;
+       
+    elseif S==2
+       a= input("Enter the slope");
+       c= input("Enter the intercepted part of y-axis ");
+       y=a.*t+c;
+       
+    elseif S==3
+       a= input("Enter the amplitude");
+       d= input("Enter the power");
+       c=input("Enter the intercepted part");
+       y=a.*(t.^d)+c;
+       
+    elseif S==4
+       a= input("Enter the amplitude");
+       e= input("Enter the exponent");
+       y=a.*exp(e.*t);
+       
+    elseif S==5
+       a= input("Enter the amplitude");
+       frq= input('Enter the frequency');
+       p= input("Enter the phase shift");
+       y=a.*sin(2*pi*frq*t+p);
+
+    else 
+       
+         disp("Not identified :)....enter no. from 1 to 5");
+          i=i;
+          continue;
+        
+    end   
+i=i+1;
+    
+    Y = [Y y];
+
+end
+plot(T,Y);
+i=1;
+while i==1 
+ disp("1.amp scaling 2.time reversal 3. time shift 4. expanding 5. compressing 6. none");
+ O= input("Enter the operation");
+ if O==1
+     a=input("Enter the amp");
+     Y=a.*Y;
+     figure;
+     plot(T,Y);
+elseif O==2
+    T=-1.*T;
+    figure;
+    plot(T,Y);
+elseif O==3
+    sh=input("Enter the time shift");
+    T=T+sh;
+    figure;
+    plot(T,Y);
+elseif O==4
+    e=input('Enter the expanding coff.');
+    T=T.*e;
+    figure;
+    plot(T,Y);
+elseif O==5
+       e=input('Enter the compressing coff.');
+     
+       for e=0
+           disp("Error division by zero");
+           break;
+       end
+           T=T./e;
+           figure;
+           plot(T,Y);
+   
+ elseif    O==6
+           for O=6
+               break;
+           end
+    
+else
+    disp("Not identified :) ... enter no. from 1 to 6");
+    i=i;
+    continue;
+ end
+i=i+1;
+end
